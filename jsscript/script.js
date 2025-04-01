a6d1058c170feb317df8ccbb3dea6189d8b61c36
Clemi <c.zangenfeind@students.htl-leonding.ac.at>
2025-04-01 20:39:52 +0200

    feat: added + button and started on the task adding interface

--- jsscript/script.js
+++ jsscript/script.js
@@ -10,3 +10,42 @@ toggleBtn.addEventListener('click', function () {
     icon.classList.replace('fa-sun-o', 'fa-moon-o');
   }
 });
+function addTab() {
+
+  document.querySelector("#topHeader").innerHTML = `
+      <button id="toggleModeBtn"><i class="fa fa-moon-o"></i></button>
+      <p id="addTaskButton" onclick="removeTabEntry()">-</p>
+  `
+  document.querySelector("#addTaskButton").innerHTML = "-";
+  if (document.querySelector('#addTaskUI') == null){
+    document.querySelector('body').innerHTML += `
+  <div id="addTaskUI">
+    <p>Input Task Name:</p>
+    <input type="text" name="nameinput" id="nameInputField" class="input">
+    <p>Input Task Description:</p>
+    <input type="text" name="descinput" id="descInputField" class="input">
+    <br>
+    <input type="submit" value="add">
+  </div>
+  `
+  } else{
+    document.querySelector("#addTaskUI").innerHTML = `
+         <div id="addTaskUI">
+          <p>Input Task Name:</p>
+          <input type="text" name="nameinput" id="nameInputField" class="input">
+          <p>Input Task Description:</p>
+          <input type="text" name="descinput" id="descInputField" class="input">
+          <br>
+          <input type="submit" value="add">
+        </div>
+`;
+  }
+
+}
+function removeTabEntry(){
+  document.querySelector("#topHeader").innerHTML = `
+      <button id="toggleModeBtn"><i class="fa fa-moon-o"></i></button>
+      <p id="addTaskButton" onclick="addTab()">+</p>
+  `
+  document.querySelector('#addTaskUI').innerHTML = "";
+}
