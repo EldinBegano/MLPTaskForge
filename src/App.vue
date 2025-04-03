72e0659b0fed06272d33c272a3b8babd923ceb05
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-04-03 15:22:04 +0200

    fix: move layout from index.html to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,51 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
 
-const greetMsg = ref("");
-const name = ref("");
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
-}
 </script>
 
 <template>
-  <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
+  <body>
+  <div id="topHeader" class="header">
+    <button id="toggleModeBtn"><i class="fa fa-moon-o"></i></button>
 
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+    <div id="addTaskWrapper">
+      <p id="addTaskButton" onclick="addTab()">+</p>
     </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
+  </div>
 
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
-  </main>
+  <div class="todo-container">
+    <div class="board-header">
+      <div style="width: 4.87%; border-right: 1px solid #000"></div>
+
+      <div class="column-title">To Do</div>
+
+      <div class="column-title">In Progress</div>
+
+      <div class="column-title">Done</div>
+    </div>
+
+    <div class="content-wrapper">
+      <div class="side-labels">
+        <div class="board-label">Board 1</div>
+      </div>
+
+      <div class="board-content">
+        <div class="board-column">
+          <div class="card">
+            <div class="card-title">Projekt machen</div>
+
+            <div class="card-details">Style testen</div>
+          </div>
+
+          <div class="empty-card"></div>
+        </div>
+
+        <div class="board-column"></div>
+
+        <div class="board-column"></div>
+      </div>
+    </div>
+  </div>
+  </body>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,20 @@ import App from "./App.vue";
 
 createApp(App).mount("#app");
 
-const toggleBtn = document.getElementById('toggleModeBtn') as HTMLButtonElement | null;
-
-if (toggleBtn) {
-  const icon = toggleBtn.querySelector('i') as HTMLElement | null;
-
-  toggleBtn.addEventListener('click', function () {
-    document.body.classList.toggle('dark-mode');
-
-    if (icon) {
-      if (icon.classList.contains('fa-moon-o')) {
-        icon.classList.replace('fa-moon-o', 'fa-sun-o');
-      } else {
-        icon.classList.replace('fa-sun-o', 'fa-moon-o');
-      }
-    }
-  });
-}
+// const toggleBtn = document.getElementById('toggleModeBtn') as HTMLButtonElement | null;
+//
+// if (toggleBtn) {
+//   const icon = toggleBtn.querySelector('i') as HTMLElement | null;
+//
+//   toggleBtn.addEventListener('click', function () {
+//     document.body.classList.toggle('dark-mode');
+//
+//     if (icon) {
+//       if (icon.classList.contains('fa-moon-o')) {
+//         icon.classList.replace('fa-moon-o', 'fa-sun-o');
+//       } else {
+//         icon.classList.replace('fa-sun-o', 'fa-moon-o');
+//       }
+//     }
+//   });
+// }
